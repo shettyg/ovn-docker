3c57cb0430ae54792f61a03c740d726bc5a41b9b
Bhargav Bhikkaji <bharbhi@gmail.com>
2015-07-24 15:33:54 -0700

    Updated error message while network create and delete on test1.sh
    Signed-off-by: Bhargav Bhikkaji <bharbhi@gmail.com>

--- vagrant_overlay/test1.sh
+++ vagrant_overlay/test1.sh
@@ -6,13 +6,13 @@ neutron net-create $NID --tenant-id admin --os-url http://$IPAM_IP:9696/ --os-au
 neutron subnet-create $NID 192.168.1.0/24 --tenant-id admin --os-url http://$IPAM_IP:9696/ --os-auth-strategy="noauth"
 
 if docker network ls | grep foo 2>&1 >/dev/null; then :; else
-  echo "test failed with no network seen"
+  echo "test failed while creating network"
 fi
 
 docker network rm foo
 
 if docker network ls | grep foo 2>&1 >/dev/null; then
-  echo "test failed with no network seen"
+  echo "test failed while deleting network"
 fi
 
 if neutron net-show $NID --tenant-id admin --os-url http://$IPAM_IP:9696/ --os-auth-strategy="noauth" 2>/dev/null; then
