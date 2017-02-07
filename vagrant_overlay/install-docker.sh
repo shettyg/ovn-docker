1d1b51721bba7db5ae04b04a49b8abc7b90af0e3
Gurucharan Shetty <guru@ovn.org>
2017-02-07 15:25:38 -0800

    vagrant: Docker defaults location has changed.
    With Docker 1.13, the default place to configure
    defaults is /etc/docker/daemon.json
    
    Tested with Docker 1.13 and the vagrant works.
    
    Signed-off-by: Gurucharan Shetty <guru@ovn.org>

--- vagrant_overlay/install-docker.sh
+++ vagrant_overlay/install-docker.sh
@@ -1,7 +1,13 @@
 IP=$1
 service docker stop
 wget -qO- https://get.docker.com/ | sh
-DOCKER_OPTS="--cluster-store=consul://127.0.0.1:8500 --cluster-advertise=$IP:0"
-echo DOCKER_OPTS=\"$DOCKER_OPTS\" >> /etc/default/docker
+
+cat <<EOF >> /etc/docker/daemon.json
+{
+    "cluster-advertise": "$1:0",
+    "cluster-store": "consul://127.0.0.1:8500"
+}
+EOF
+
 
 service docker restart
